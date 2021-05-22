a473d6cd79d815affaf5007be9a1933e65cbd0ab
Abel AR <abel_john_14@hotmail.com>
2021-05-21 23:59:43 -0500

    Add names to the navbar and links the views

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -4,6 +4,10 @@
     class="sticky flex items-center p-3 space-x-3 bg-blue-500 w-full h-14"
   >
     <router-link to="/">Home</router-link>
+    <router-link to="/users">Users</router-link>
+    <router-link to="/locations">Locations</router-link>
+    <router-link to="/turn-templates">Turn templates</router-link>
+    <router-link to="/calendario">Calendar</router-link>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import Calendar from '../views/Calendar.vue';
+import Locations from '../views/Locations.vue';
+import TurnTemplates from '../views/TurnTemplates.vue';
+import User from '../views/Users.vue';
 
 Vue.use(VueRouter);
 const routes = [
@@ -14,6 +17,21 @@ const routes = [
     path: '/calendario',
     name: 'Calendar',
     component: Calendar,
+  },
+  {
+    path: '/locations',
+    name: 'Locations',
+    component: Locations,
+  },
+  {
+    path: '/turn-templates',
+    name: 'Turn templates',
+    component: TurnTemplates,
+  },
+  {
+    path: '/users',
+    name: 'Users',
+    component: User,
   },
 ];
 
